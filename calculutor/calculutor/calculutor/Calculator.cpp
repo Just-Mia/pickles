8a541088794df7f5343c6a5e19c34bf34650124c
Just-Mia <solomiia.petriuk@gmail.com>
2025-11-11 18:47:15 +0200

    feature-Petriuk commit

--- calculutor/calculutor/calculutor/Calculator.cpp
+++ calculutor/calculutor/calculutor/Calculator.cpp
@@ -18,9 +18,11 @@ int Calculator::subtract(int a, int b) {
 }
 
 int Calculator::multiply(int a, int b) {
+    return a * b;
 }
 
 double Calculator::divide(int a, int b) {
+    return a / b;
 }
 
 int Calculator::power(int base, int exp) {
